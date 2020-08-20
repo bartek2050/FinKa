fcc55ede19a9df91c9c55c5db9101b66a8902923
bartek2050 <bartlomiej.ciastek@gmail.com>
2020-08-21 01:23:07 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
 import React from 'react';
 import './scss/main.scss'
 
+const App = () => {
+
+
+    return (
+        <header className={"header"}>
+            <div className={"header__box"}>
+                <a href={"/"} className={"logo"}>FinKa +</a>
+                <nav className={"header__navigation"}>
+                    <li className={"navigation__list"}>
+                        <ul><a href={"/"} className={"navigation__list-element"}>EXCHANGE</a></ul>
+                        <ul><a href={"/"} className={"navigation__list-element"}>CRYPTO</a></ul>
+                        <ul><a href={"/"} className={"navigation__list-element"}>NEWS</a></ul>
+                    </li>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default App
+
 
